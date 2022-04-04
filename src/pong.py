873bc8df91e2a29ae4cf29fb0b5c3cc43b064414
Laura Peltonen <laura.peltonen53@gmail.com>
2022-04-04 22:00:37 +0300

    test picture

--- src/pong.py
+++ src/pong.py
@@ -1,9 +1,18 @@
 import pygame
+import os
+
+dirname = os.path.dirname(__file__)
 
 pygame.init()
 width = 1200
-height = 990
+height = 880
 screen = pygame.display.set_mode((width,height))
+
+ball = pygame.image.load(os.path.join(dirname, "objects", "red_ball.png"))
+
+screen.fill((118,238,198))
+screen.blit(ball, (100, 80))
+pygame.display.flip()
 
 while True:
 	for event in pygame.event.get():
