67cbfd22ffd50dccd31a68fb8ad71599e1a8d768
Laura Peltonen <laura.peltonen53@gmail.com>
2022-04-04 23:02:23 +0300

    fixing pong.py and created new ball

--- src/pong.py
+++ src/pong.py
@@ -4,17 +4,30 @@ import os
 dirname = os.path.dirname(__file__)
 
 pygame.init()
-width = 1200
+width = 880
 height = 880
 screen = pygame.display.set_mode((width,height))
 
 ball = pygame.image.load(os.path.join(dirname, "objects", "red_ball.png"))
 
-screen.fill((118,238,198))
-screen.blit(ball, (100, 80))
-pygame.display.flip()
+x = 0
+y = 0
+speed = 4
+clock = pygame.time.Clock()
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			exit()
+			
+	screen.fill((118,238,198))
+	screen.blit(ball, (x, y))
+	pygame.display.flip()
+
+	x += speed
+	if speed > 0 and x + ball.get_width() >= 880:
+		speed =- speed
+	if speed < 0 and x <= 0:
+		speed =- speed
+		
+	clock.tick(60)
