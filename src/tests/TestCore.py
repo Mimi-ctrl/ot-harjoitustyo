33e24c4f68bca88aed2b433a345b664175b71c72
Laura Peltonen <laura.peltonen53@gmail.com>
2022-04-11 23:50:09 +0300

    test base

---
+++ src/tests/TestCore.py
@@ -0,0 +1,13 @@
+import unittest
+from Pong import Points
+
+class TestCore(unittest.TestCase):
+    def setUp(self):
+        self.board1 = Points(0)
+        self.board2 = Points(0)
+
+   # def test_boar1_get_points_and_board2_not():
+
+   # def test_boar2_get_points_and_board1_not():
+
+   # def test_poits_reset_when_twenty():
