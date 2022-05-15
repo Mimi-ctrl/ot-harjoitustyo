bcff986be97e8390c4283b859bdb01b099f9902d
Laura Peltonen <laura.peltonen53@gmail.com>
2022-05-15 22:31:47 +0300

    test

--- src/tests/boards_max_positions_test.py
+++ src/tests/boards_max_positions_test.py
@@ -1,25 +1,27 @@
 import unittest
-from sprites import board
+from game_loop import BOARD1, BOARD2, boards_max_positions
 
 class TestBoardPosition(unittest.TestCase):
     def setUp(self):
-        self.b1 = board.Board1.self.rect.y(-1)
-        self.b2 = board.Board2.self.rect.y(-1)
-        self.b11 = board.Board1.self.rect.y(526)
-        self.b22 = board.Board2.self.rect.y(526)
+        self.b1 = BOARD1
+        self.b2 = BOARD2
 
     def test_board1_down_max_position_is_right(self):
-        self.b1.board_max_positions()
-        self.assertEqual(self.b1,0)
+        self.b1.rect.y = -1
+        boards_max_positions()
+        self.assertEqual(self.b1.rect.y,0)
     
     def test_board2_down_max_position_is_right(self):
-        self.b2.board_max_positions()
-        self.assertEqual(self.b2,0)
+        self.b2.rect.y = -1
+        boards_max_positions()
+        self.assertEqual(self.b2.rect.y,0)
 
     def test_board1_up_max_position_is_right(self):
-        self.b11.board_max_positions()
-        self.assertEqual(self.b11,525)
+        self.b1.rect.y = 526
+        boards_max_positions()
+        self.assertEqual(self.b1.rect.y,525)
 
     def test_board2_up_max_position_is_right(self):
-        self.b22.board_max_positions()
-        self.assertEqual(self.b22,525)
+        self.b2.rect.y = 526
+        boards_max_positions()
+        self.assertEqual(self.b2.rect.y,525)
