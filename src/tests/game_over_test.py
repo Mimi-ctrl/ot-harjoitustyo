4bb3110bcd4f29f3aecbeeb6ce720a807407bcdd
Laura Peltonen <laura.peltonen53@gmail.com>
2022-05-15 22:01:43 +0300

    -

--- src/tests/game_over_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from game_loop import board1, board2
-
-class TestGameOver(unittest.TestCase):
-    def setUp(self):
-        self.b1 = board1.points(19)
-        self.b2 = board2.points(19)
-
